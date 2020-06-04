6061e5218f9446d21af9466f1495844c135efa79
MadByteLife <germans1998@gmail.com>
2020-06-04 20:01:03 +0300

    added [object Object]

--- server/router.js
+++ server/router.js
@@ -5,6 +5,14 @@ const bcrypt = require("bcrypt");
 const express = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 function checkAuth(req, res, next) {
   passport.authenticate(
@@ -26,12 +34,120 @@ function createToken(body) {
   });
 }
 
+
+
 module.exports = app => {
   app.use("/assets", express.static("./client/public"));
+  
+  app.use(bodyParser.json());
+  app.use(methodOverride('_method'));
+
+  const conn = mongoose.createConnection(config.mongo.url, config.mongo.options);
+
+  let gfs;
+
+  conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+  })
+
+  const storage = new GridFsStorage({
+    url: 'mongodb://localhost:27017/Margelet',
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    } 
+  });
+  const upload = multer({ storage });
+
+  // POST /upload
+  // Uploads file to DB
+  app.post('/upload', upload.single('file'), (req, res) => {
+    //res.json({file: req.file});
+    res.redirect('/');
+  });
+
+  // GET /files
+  // Display all files in JSON
+  app.get('/files', (req, res) => {
+   gfs.files.find().toArray((err, files) => {
+     // Check if files
+      if (!files || files.length === 0) {
+       return res.status(404).json({
+          err: 'No files exist'
+        });
+      }
+      return res.json(files);
+    });
+  });
+
+  // GET /files/:filename
+  // Display single file object
+  app.get('/files/:filename', (req, res) => {
+   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+     // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+         err: 'No file exists'
+        });
+      }
+     // File exists
+     return res.json(file);
+    });
+  });
+
+  // GET /image/:filename
+  // Display Image
+  app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+    
+      // Check if image
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+       res.status(404).json({
+         err: 'Not an image'
+        });
+     }
+   });
+  });
 
   app.get("/", checkAuth, (req, res) => {
-    res.render("index.html", { username: req.user.username });
+    gfs.files.find().toArray((err, files) => {
+      //Check if files
+      if (!files || files.length === 0) {
+        res.render("index.html", { username: req.user.username, files:false });
+      } else {
+        files.map(file => {
+          if(file.contentType ==='image/jpeg' || file.contentType === 'image/png') {
+            file.isImage = true;
+          } else {
+            file.isImage = false;
+        }
+      });
+      res.render("index.html", { username: req.user.username, files: files });
+      }
+    });
   });
+
 
   app.post("/login", async (req, res) => {
     try {
--- server/server.js
+++ server/server.js
@@ -9,13 +9,6 @@ const io = require("socket.io")(server, { serveClient: true });
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const config = require("./config");
-const path = require('path');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-const methodOverride = require('method-override');
 
 const passport = require("passport");
 const { Strategy } = require("passport-jwt");
@@ -43,68 +36,12 @@ nunjucks.configure("./client/views", {
   express: app
 });
 
-app.use(bodyParser.json());
-app.use(methodOverride('_method'));
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 // parse application/json
 app.use(express.json());
 
 app.use(cookieParser());
-
-let gfs;
-const conn = mongoose.createConnection(config.mongo.url, config.mongo.options);
-conn.once('open', () => {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
-})
-
-const storage = new GridFsStorage({
-  url: 'mongodb://localhost:27017/Margelet',
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const upload = multer({ storage });
-
-app.post('/upload', upload.single('file'), (req, res) => {
-  //res.json({file: req.file});
-  res.redirect('/');
-});
-
-app.get('/files', (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist'
-      });
-    }
-    return res.json(files);
-  });
-});
-
-app.get('/files/:filename', (req, res) => {
-  gfs.files.findOne({filename: req.params.filename}).toArray((err, files) => {
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist'
-      });
-    }
-    return res.json(files);
-  });
-});
 
 require("./router")(app);
 
