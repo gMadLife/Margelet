c0962ff5445dc723dbf62285a7e493e061d334d0
andreasxp <andres.zhukov@gmail.com>
2020-06-07 10:14:12 +0300

    Remove obsolete code

--- server/router.js
+++ server/router.js
@@ -6,11 +6,6 @@ const bcrypt = require("bcrypt");
 const express = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-const path = require('path');
-const crypto = require('crypto');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
@@ -36,8 +31,6 @@ function createToken(body) {
   });
 }
 
-
-
 module.exports = app => {
   app.use("/assets", express.static("./client/public"));
   
@@ -46,69 +39,6 @@ module.exports = app => {
 
   const conn = mongoose.createConnection(config.mongo.url, config.mongo.options);
 
-  let gfs;
-
-  conn.once('open', () => {
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-  })
-
-  const storage = new GridFsStorage({
-    url: 'mongodb://localhost:27017/Margelet',
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        crypto.randomBytes(16, (err, buf) => {
-          if (err) {
-            return reject(err);
-          }
-          const filename = buf.toString('hex') + path.extname(file.originalname);
-          const fileInfo = {
-            filename: filename,
-            bucketName: 'uploads'
-          };
-          resolve(fileInfo);
-        });
-      });
-    } 
-  });
-  const upload = multer({ storage });
-
-  // POST /upload
-  // Uploads file to DB
-  app.post('/upload', upload.single('file'), (req, res) => {
-    //res.json({file: req.file});
-    res.redirect('/');
-  });
-
-  // GET /files
-  // Display all files in JSON
-  app.get('/files', (req, res) => {
-   gfs.files.find().toArray((err, files) => {
-     // Check if files
-      if (!files || files.length === 0) {
-       return res.status(404).json({
-          err: 'No files exist'
-        });
-      }
-      return res.json(files);
-    });
-  });
-
-  // GET /files/:filename
-  // Display single file object
-  app.get('/files/:filename', (req, res) => {
-   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-     // Check if file
-      if (!file || file.length === 0) {
-        return res.status(404).json({
-         err: 'No file exists'
-        });
-      }
-     // File exists
-     return res.json(file);
-    });
-  });
-
   app.get('/uploads/:filename', (req, res) => {
     UploadsModel.findById(req.params.filename, (err, file) => {
       if (!file) {
@@ -116,8 +46,6 @@ module.exports = app => {
           err: "No file exists",
         });
       }
-
-      console.log("##########" + contentDisposition(file.filename));
 
       res.writeHead(200, {
         'Content-Type': "application/octet-stream",
@@ -130,47 +58,9 @@ module.exports = app => {
     });
   });
 
-  // GET /image/:filename
-  // Display Image
-  app.get('/image/:filename', (req, res) => {
-    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-      // Check if file
-      if (!file || file.length === 0) {
-        return res.status(404).json({
-          err: 'No file exists'
-        });
-      }
-    
-      // Check if image
-      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-        const readstream = gfs.createReadStream(file.filename);
-        readstream.pipe(res);
-      } else {
-       res.status(404).json({
-         err: 'Not an image'
-        });
-     }
-   });
-  });
-
   app.get("/", checkAuth, (req, res) => {
-    gfs.files.find().toArray((err, files) => {
-      //Check if files
-      if (!files || files.length === 0) {
-        res.render("index.html", { username: req.user.username, files: false });
-      } else {
-        files.map(file => {
-          if(file.contentType ==='image/jpeg' || file.contentType === 'image/png') {
-            file.isImage = true;
-          } else {
-            file.isImage = false;
-        }
-      });
-      res.render("index.html", { username: req.user.username, files: files });
-      }
-    });
+    res.render("index.html", { username: req.user.username });
   });
-
 
   app.post("/login", async (req, res) => {
     try {
